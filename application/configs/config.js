f5be30403b97babf0211556d3fcbcd34a0250fed
Wallysson Nunes <wally.nm@gmail.com>
2015-08-25 22:44:50 -0300

    Updated mongodb url

--- application/configs/config.js
+++ application/configs/config.js
@@ -12,7 +12,7 @@ var config = {
   prod: {
     mode: 'prod',
     port: process.env.PORT || 3010,
-    mongoUrl: 'ds035593.mongolab.com:35593/heroku_cn6vx9lj'
+    mongoUrl: 'mongodb://heroku_cn6vx9lj:kat601c5tfh46e0674vp72r0ph@ds035593.mongolab.com:35593/heroku_cn6vx9lj'
   },
 }
 
