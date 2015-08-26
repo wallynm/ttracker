ed7ba619c2cfa33d42a1b54afb6fb8f4bb12c6d3
Wallysson Nunes <wally.nm@gmail.com>
2015-08-25 23:39:12 -0300

    Atualizado config.js

--- application/configs/config.js
+++ application/configs/config.js
@@ -2,19 +2,19 @@ var config = {
   local: {
     mode: 'local',
     port: 3000,
-    mongoUrl: 'mongodb://localhost:27017/ttracker'
+    databaseConnection: 'mongodb://localhost:27017/ttracker'
   },
   staging: {
     mode: 'staging',
     port: 3005,
-    mongoUrl: 'mongodb://localhost:27017/ttracker'
+    databaseConnection: 'mongodb://localhost:27017/ttracker'
   },
   prod: {
     mode: 'prod',
     port: process.env.PORT || 3010,
-    mongoUrl: 'mongodb://localhost:27017/ttracker'
-  },
-}
+    databaseConnection: 'mongodb://heroku_cn6vx9lj:kat601c5tfh46e0674vp72r0ph@ds035593.mongolab.com:35593/heroku_cn6vx9lj'
+  }
+};
 
 module.exports = function(mode) {
   return config[mode || process.argv[2] || 'local'] || config.local;
